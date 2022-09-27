e652e95fe3adabbaaf7d9f27abdd31993ce367bb
Samuel José Haro <samuelharo1@gmail.com>
2022-09-27 04:45:42 -0400

    feat: add languages
    -add English and Spanish

--- src/components/welcome/index.jsx
+++ src/components/welcome/index.jsx
@@ -1,21 +1,33 @@
 import styles from "./styles.module.css"
 import { useRouter } from "next/router"
+import { useContext } from "react"
+import AppContext from "src/context/AppContext"
+import scripts from "./languages"
 
 const MY_SUMMONER_QUERY = "la1/Rolling+Typhoon"
 
 export default function Welcome() {
   const router = useRouter()
+  const { language } = useContext(AppContext)
+  const script = scripts[language] 
+
+  if (!script) return <></>
   return (
     <div className={styles.welcome}>
       <div className={styles.welcome__title}>
-        <h1>{"Welcome to League's Mastery"}</h1>
+        <h1>{script.title}</h1>
       </div>
       <div className={styles.welcome__desc}>
-        <h2>{"Search a summoner's profile"}</h2>
-        <p>{"Or..."}</p>
-        <button onClick={()=>{
-          router.push(`/summoners/${MY_SUMMONER_QUERY}`)
-        }} className={styles["welcome__desc--button"]}>{"Search me!"}</button>
+        <h2>{script.desc}</h2>
+        <p>{script.meeting[1]}</p>
+        <button
+          onClick={() => {
+            router.push(`/summoners/${MY_SUMMONER_QUERY}`)
+          }}
+          className={styles["welcome__desc--button"]}
+        >
+          {script.meeting[2]}
+        </button>
       </div>
     </div>
   )
---
+++ src/components/welcome/languages.js
@@ -0,0 +1,22 @@
+export const en_US = {
+  title: "Welcome to League's Mastery",
+  desc: "Search a summoner's profile",
+  meeting: {
+    1: "or...",
+    2: "search me!",
+  },
+}
+
+export const es_MX = {
+  title: "Bienvenido a League's Mastery",
+  desc: "Busca un perfil de invocador",
+  meeting: {
+    1: "o...",
+    2: "¡búscame!",
+  },
+}
+
+export default {
+  es_MX,
+  en_US,
+}
