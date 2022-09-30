b2f04400d3866d266410e71cc93508aa93f37b50
Samuel José Haro <samuelharo1@gmail.com>
2022-09-30 13:10:36 -0400

    feat: add layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,81 @@
 import "../styles/normalize.css"
 import "../styles/globals.css"
 import "styles/fontawesome/css/all.css"
+import SummonerSearcher from "src/components/summonerSearcher"
+import ConfigMenu from "src/components/configMenu"
+import { useContext } from "react"
 
-import { AppProvider } from "src/context/AppContext"
+import AppContext, { AppProvider } from "src/context/AppContext"
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AppProvider>
+  )
+}
+
+function Layout({ children }) {
+  // TODO: Find a better place for it
+  const { language } = useContext(AppContext)
+  
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            flexGrow: 1,
+          }}
+        >
+          <SummonerSearcher />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            backgroundColor: "var(--cardBack)",
+          }}
+        >
+          <button
+            style={{
+              backgroundColor: "var(--btnBack)",
+              color: "var(--btnText)",
+              padding: "10px",
+              borderRadius: "15px",
+              borderWidth: "1px",
+              borderColor: "transparent",
+              // backgroundColor: "#fffffe",
+            }}
+            type="button"
+          >
+            <i className="fa-solid fa-screwdriver-wrench" />
+          </button>
+        </div>
+      </div>
+      <div
+        style={{
+          flexGrow: 1,
+        }}
+      >
+        {children}
+        <div>
+          <ConfigMenu language={language} />
+        </div>
+      </div>
+    </div>
   )
 }
 
